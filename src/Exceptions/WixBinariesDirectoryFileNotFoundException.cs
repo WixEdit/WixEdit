a37c2bef01600e08a8b762330c7b9102c50f8134
Rami <icnocop@users.noreply.github.com>
2023-10-08 18:41:45 -0700

    Consolidated duplicate code

---
+++ src/Exceptions/WixBinariesDirectoryFileNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace WixEdit.src.Exceptions
+{
+    internal class WixBinariesDirectoryFileNotFoundException : WixEditException
+    {
+        public WixBinariesDirectoryFileNotFoundException(string fileName)
+            : base($"The executable \"{fileName}\" could not be found.\r\n\r\nPlease specify the correct path to the Wix binaries in the settings dialog.")
+        {
+        }
+    }
+}
